3da4f8b6cd2ebe95bd7e4e2295080e77f5243a30
khadar786 <khadarsayyad@gmail.com>
2024-08-02 18:58:36 +0530

    add api calls

--- ipl-api-service/app.py
+++ ipl-api-service/app.py
@@ -11,6 +11,7 @@ app.secret_key='IPLAPISERVICE#2024'
 
 #teamrecord
 #http://127.0.0.1:5000/api/team-record?team=Gujarat%20Titans
+#players
 @app.route('/')
 def index():
   return 'Hi'
@@ -19,8 +20,12 @@ def index():
 def teams():
   teams=ipl.teamsAPI()
   return jsonify(teams)
-  
 
+@app.route('/api/players')
+def players():
+  response=ipl.players()
+  return jsonify(response)
+  
 @app.route('/api/teamvteam')
 def teamvteam():
   team1=request.args.get('team1')
@@ -28,16 +33,19 @@ def teamvteam():
   response=ipl.team_vs_team(team1,team2)
   return jsonify(response)
 
-@app.route('/api/players')
-def players():
-  response=ipl.players()
+@app.route('/api/team-record')
+def teamrecord():
+  team=request.args.get('team')
+  response=ipl.teamRecord(team)
   return jsonify(response)
 
-@app.route('/api/team-record')
-def allRecord():
-  team=request.args.get('team')
-  response=ipl.allRecord(team)
-  return jsonify(response)
+@app.route('/api/batting-record')
+def batting_record():
+  pass
+
+@app.route('/api/bowling-record')
+def bowling_record():
+  pass
 
 if __name__=='__main__':
   app.run(debug=True)
--- ipl-api-service/ipl.py
+++ ipl-api-service/ipl.py
@@ -1,9 +1,27 @@
 import pandas as pd
 import numpy as np
+import json
 
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+      
 ipl_matches='https://docs.google.com/spreadsheets/d/e/2PACX-1vRy2DUdUbaKx_Co9F0FSnIlyS-8kp4aKv_I0-qzNeghiZHAI_hw94gKG22XTxNJHMFnFVKsO4xWOdIs/pub?gid=1655759976&single=true&output=csv'
 matches=pd.read_csv(ipl_matches)
 
+ipl_ball = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRu6cb6Pj8C9elJc5ubswjVTObommsITlNsFy5X0EiBY7S-lsHEUqx3g_M16r50Ytjc0XQCdGDyzE_Y/pub?output=csv"
+balls = pd.read_csv(ipl_ball)
+
+ball_withmatch = balls.merge(matches, on='ID', how='inner').copy()
+ball_withmatch['BowlingTeam'] = ball_withmatch.Team1 + ball_withmatch.Team2
+ball_withmatch['BowlingTeam'] = ball_withmatch[['BowlingTeam', 'BattingTeam']].apply(lambda x: x.values[0].replace(x.values[1], ''), axis=1)
+batter_data = ball_withmatch[np.append(balls.columns.values, ['BowlingTeam', 'Player_of_Match'])]
 
 def teamsAPI():
   teams=list(set(list(matches['Team1'])+list(matches['Team2'])))
@@ -45,7 +63,7 @@ def players():
   }
   return players_dict
 
-def allRecord(team):
+def teamRecord(team):
     df = matches[(matches['Team1'] == team) | (matches['Team2'] == team)].copy()
     mp = df.shape[0]
     won = df[df.WinningTeam == team].shape[0]
